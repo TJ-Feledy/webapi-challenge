573f2de8bcaefc7301872c34f9f57468e81188ec
teej387 <teej387@yahoo.com>
2019-08-19 18:24:31 -0500

    Created actionRouter.js and copied over endpoints from projectRouter.js

---
+++ data/actions/actionRouter.js
@@ -0,0 +1,113 @@
+const express = require('express')
+
+const Actions = require('../helpers/actionModel.js')
+
+const router = express.Router()
+
+router.post('/:id/actions', validateProjectId, validateAction, (req, res) => {
+  const actionBody = { ...req.body, project_id: req.project.id }
+
+  Actions.insert(actionBody)
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ errorMessage: 'Error adding Action' })
+    })
+})
+
+router.get('/', (req, res) => {
+  Actions.get()
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ errorMessage: 'Error getting Actions' })
+    })
+})
+
+router.get('/:id', validateActionId, (req, res) => {
+  res.status(200).json(req.action)
+})
+
+router.delete('/:id', validateActionId, (req, res) => {
+  const { id } = req.params
+
+  Actions.remove(id)
+    .then(action => {
+      if (action === 0) {
+        res.status(404).json({ message: 'Action ID is invalid.' })
+      } else {
+        res.status(200).json(action)
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ errorMessage: 'Could not remove the Action.' })
+    })
+})
+
+router.put('/:id', validateActionId, validateAction, (req, res) => {
+  Actions.update(req.params.id, req.body)
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ errorMessage: 'Could not update the Action.' })
+    })
+})
+
+// -------Custom Middleware------
+
+function validateProjectId(req, res, next) {
+  const id = req.params.id
+
+  Projects.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project
+        next()
+      } else {
+        res.status(400).json({ message: 'invalid project id' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ errorMessage: 'Error finding project' })
+    })
+};
+
+function validateActionId(req, res, next) {
+  const id = req.params.id
+
+  Actions.get(id)
+    .then(action => {
+      if (action) {
+        req.action = action
+        next()
+      } else {
+        res.status(400).json({ message: 'invalid Action id' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ errorMessage: 'Error finding Action' })
+    })
+};
+
+function validateAction(req, res, next) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: 'missing Action data' })
+  }
+  else if (!req.body.name || req.body.name.split('').length === 0 || req.body.description.split('').length === 0) {
+    res.status(400).json({ message: 'missing required project_id and/or description' })
+  } else {
+    next()
+  }
+}
+
+
+module.exports = router
