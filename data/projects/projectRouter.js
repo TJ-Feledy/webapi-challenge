3e8836778b7e261dbc3da724058dae3dc89b8c2c
teej387 <teej387@yahoo.com>
2019-08-19 00:01:49 -0500

    Created validateProjectId() and validateProject() custom middleware

--- data/projects/projectRouter.js
+++ data/projects/projectRouter.js
@@ -6,4 +6,36 @@ const router = express.Router()
 
 
 
+// -------Custom Middleware------
+
+function validateProjectId(req, res, next) {
+  const id = req.params.id
+
+  Projects.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project
+        next()
+      } else {
+        res.status(400).json({ message: 'invalid project id' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ errorMessage: 'Error finding project' })
+    })
+};
+
+function validateProject(req, res, next) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: 'missing project data' })
+  }
+  else if (!req.body.name || req.body.name.split('').length === 0 || req.body.description.split('').length === 0) {
+    res.status(400).json({ message: 'missing required name and/or description' })
+  } else {
+    next()
+  }
+}
+
+
 module.exports = router
