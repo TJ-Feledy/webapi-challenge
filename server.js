d343cb12c2f74019e5f6edffe95a415e01861f79
teej387 <teej387@yahoo.com>
2019-08-18 23:49:39 -0500

    Created projectRouter.js and imported to server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 
+const projectRouter = require('./data/projects/projectRouter.js')
+
 const server = express()
 
 server.use(express.json())
+
+server.use('/api/projects', projectRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>This is the webapi sprint challenge!</h2>`)
